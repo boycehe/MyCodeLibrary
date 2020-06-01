988893d0a28ad7c1d9845cb2e06b84826f5b3775
boyce <peilin.hpl@alibaba-inc.com>
2020-06-01 21:30:09 +0800

    #ADD#files

---
+++ Algorithm/C/C/linkedList.h
@@ -0,0 +1,14 @@
+//
+//  linkedList.h
+//  C
+//
+//  Created by heboyce on 2020/5/31.
+//  Copyright © 2020 heboyce. All rights reserved.
+//
+
+#ifndef linkedList_h
+#define linkedList_h
+
+#include <stdio.h>
+
+#endif /* linkedList_h */
---
+++ Algorithm/C/C/search.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int len = 20;
+int data[20] = {0,12,21,34,45,56,67,70,81,91,101,112,123,135,146,150,163,173,180,191};
+/*
+二分查找
+*/
+
+int binarySearch(int a){
+	
+	int begin = 0;
+	int end = len-1;
+	int mid = (end - begin)/2;
+	while (begin <= end) {
+		
+		mid   =  (end + begin)/2;
+		if (a > data[mid]) {
+			begin = mid+1;
+		}else {
+			end  = mid-1;
+		}
+		
+	}
+	if (a == data[mid]) {
+		return mid;
+	}
+	return -1;
+}
+
