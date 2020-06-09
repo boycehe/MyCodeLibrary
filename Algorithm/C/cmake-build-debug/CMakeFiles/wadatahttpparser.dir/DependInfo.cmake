80d6481fde200f632802c042ddbc7a7d3110a954
boyce <peilin.hpl@alibaba-inc.com>
2020-06-09 14:38:29 +0800

    #ADD#增加clion支持

---
+++ Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/C/hc_question.c" "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/C/hc_question.c.o"
+  "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/C/he_linkedlist.c" "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/C/he_linkedlist.c.o"
+  "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/C/he_queue.c" "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/C/he_queue.c.o"
+  "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/C/he_search.c" "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/C/he_search.c.o"
+  "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/C/he_stack.c" "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/C/he_stack.c.o"
+  "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/C/he_tree.c" "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/C/he_tree.c.o"
+  "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/C/main.c" "/Users/heboyce/Desktop/MyCodeLibrary/Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/C/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../C"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Algorithm/C/cmake-build-debug/CMakeFiles/wadatahttpparser.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wadatahttpparser.dir/C/hc_question.c.o"
+  "CMakeFiles/wadatahttpparser.dir/C/he_linkedlist.c.o"
+  "CMakeFiles/wadatahttpparser.dir/C/he_queue.c.o"
+  "CMakeFiles/wadatahttpparser.dir/C/he_search.c.o"
+  "CMakeFiles/wadatahttpparser.dir/C/he_stack.c.o"
+  "CMakeFiles/wadatahttpparser.dir/C/he_tree.c.o"
+  "CMakeFiles/wadatahttpparser.dir/C/main.c.o"
+  "libwadatahttpparser.a"
+  "libwadatahttpparser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/wadatahttpparser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
